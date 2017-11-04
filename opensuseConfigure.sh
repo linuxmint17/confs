42dcaf0b8775ca321500d70d8e87a11c599b8e1e
suse <opensuselinux13.2@gmail.com>
2017-11-04 20:36:27 +0800

    add cflags for opencv compile

--- opensuseConfigure.sh
+++ opensuseConfigure.sh
@@ -8,6 +8,7 @@ sudo zypper addrepo https://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-
 sudo zypper refresh
 sudo zypper install --non-interactive google-chrome-stable
 #this part is for sublimetext3 ;can call it by subl from terminal
-sudo rm - --import "https://download.sublimetext.com/sublimehq-rpm-pub.gpg"
+sudo rpm  --import "https://download.sublimetext.com/sublimehq-rpm-pub.gpg"
 sudo zypper addrepo -g -f https://download.sublimetext.com/rpm/stable/x86_64/sulime-text.repo
+
 sudo zypper install sublime-text
