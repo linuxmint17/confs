3a402f9c471b55af654aa4d6e95a5967b9630f96
wangshaodong <shaodongwang@gmail.com>
2022-01-14 02:21:42 +0800

    fix errs

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 mkdir -p ~/.vim
-cp ./vimrc ~/.vimrc
+cp vimrc ~/.vimrc
 
-cp ./git-prompt.sh ~/.git-prompt.sh
-cp ./git-completions.bash ~/.git-completoins.bash
+cp git-prompt.sh ~/.git-prompt.sh
+cp git-completion.bash ~/.git-completion.bash
 
-cp ./alias ~/.alias
-cp ./bashrc ~/.bashrc
-cp ./profile ~/.profile
+cp alias ~/.alias
+cp bashrc ~/.bashrc
+cp profile ~/.profile
 
-cp ./git-commit-template.txt ~/.git-commit-template.txt
-cp ./gitconfig ~/.gitconfig
+cp git-commit-template.txt ~/.git-commit-template.txt
+cp gitconfig ~/.gitconfig
 
 mkdir -p ~/.ssh
-cp ./publiic-keys/* ~/.ssh/*
-cp ./ssh-config ~/.ssh/config
+cp public-keys/* ~/.ssh/
+touch ~/.ssh/config
+cp ssh-config ~/.ssh/config
+
